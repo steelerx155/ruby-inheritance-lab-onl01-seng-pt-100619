31cfae470b5b0c488f9fb725ddd0e958fc1082c6
Randy Steele <steelerx155@gmail.com>
2019-11-13 19:27:40 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,12 @@
 class User
   
-  attr_accessor 
+  attr_accessor :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  
+  
+end
